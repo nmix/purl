df81141792028f440382ef65a809568e2fbb8dd1
Nikolay Mikhaylichenko <nn.mikh@yandex.ru>
2017-08-31 16:24:35 +0300

    fix deploy 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,4 +4,4 @@ set :repo_url, 'git@bitbucket.org:zoidenberg/purl.git'
 set :deploy_to, '/home/deploy/purl'
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin db log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
