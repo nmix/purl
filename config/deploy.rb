2186bc3acbf9daeb991d0ddee8ef66b7c770dc91
Nikolay Mikhaylichenko <nn.mikh@yandex.ru>
2017-08-31 16:25:51 +0300

    fix deploy 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,5 +3,5 @@ set :repo_url, 'git@bitbucket.org:zoidenberg/purl.git'
 
 set :deploy_to, '/home/deploy/purl'
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin db log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml config/secrets.yml db/production.sqlite3}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
